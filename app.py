b26dea5de36730f69314ece66a4d22e6e60af292
jmainzy <78809349+jmainzy@users.noreply.github.com>
2022-01-05 17:10:25 -0500

    add routes and requirements

--- app.py
+++ app.py
@@ -1,7 +1,43 @@
-from flask import Flask
+from flask import Flask, request, abort
+from flask_restful import Resource, Api
+from marshmallow import Schema, fields, post_load
+
+# basic Search class
+class SearchQuery:
+    def __init__(self, query):
+        self.query = query
+
+# arguments for the search query
+class SearchQuerySchema(Schema):
+    query = fields.Str(required=True)
+
+    # deserializing the object
+    @post_load
+    def make_user(self, data, **kwargs):
+        return SearchQuery(**data)
+
 app = Flask(__name__)
+api = Api(app)
+schema = SearchQuerySchema()
+
+# define the api, validate, return results
+class SearchAPI(Resource):
+    def get(self):
+        errors = schema.validate(request.args)
+        if errors:
+            abort(400, str(errors))
+        searchObject = schema.load(request.args)
+        return do_search(searchObject)
+
 @app.route("/")
-def hello():
+def index():
     return "<h1 style='color:green'>Welcome to the Mvskoke Language API!</h1>"
+
+api.add_resource(SearchAPI, '/search', endpoint = 'search')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
+
+# do the actual search
+def do_search(searchObject: SearchQuerySchema):
+    return "you searched for "+searchObject.query
